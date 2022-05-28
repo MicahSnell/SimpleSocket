4c16bc6584eed4ccf32be82d8b11602e4909673a
Micah Snell <snellmicah@gmail.com>
2022-05-28 13:42:23 -0500

    replaced tabs with spaces

--- src/Socket.h
+++ src/Socket.h
@@ -25,79 +25,79 @@ public:
     MAX_PROTOCOLS
   };
       
-	/**
-	 * @brief initializes a host socket
-	 * @param portNum the port number the socket will bind to
-	 * @param protocol the connection type of the socket
-	 */
+  /**
+   * @brief initializes a host socket
+   * @param portNum the port number the socket will bind to
+   * @param protocol the connection type of the socket
+   */
   Socket (int portNum, eProtocol protocol);
 
-	/**
-	 * @brief initializes a client socket
-	 * @param hostStr the hostname or ip to connect to
-	 * @param portNum the port number on the host to connect to
-	 * @param protocol the connection type of the socket
-	 */
+  /**
+   * @brief initializes a client socket
+   * @param hostStr the hostname or ip to connect to
+   * @param portNum the port number on the host to connect to
+   * @param protocol the connection type of the socket
+   */
   Socket (std::string hostStr, int portNum, eProtocol protocol);
 
-	/**
-	 * @brief closes any open file descriptors
-	 */
+  /**
+   * @brief closes any open file descriptors
+   */
   ~Socket ();
 
-	/**
-	 * @brief attempts to connect socket to client/host
-	 * @return true if successful
-	 */
+  /**
+   * @brief attempts to connect socket to client/host
+   * @return true if successful
+   */
   bool ConnectSocket ();
  
-	/**
-	 * @brief attempts to send data given in buffer
-	 * @param buffer a pointer to the data to be sent
-	 * @param numBytes the number of bytes to be sent
-	 * @return true if data is sent successfully
-	 */
+  /**
+   * @brief attempts to send data given in buffer
+   * @param buffer a pointer to the data to be sent
+   * @param numBytes the number of bytes to be sent
+   * @return true if data is sent successfully
+   */
   bool Send (const void *buffer, int numBytes);
 
-	/**
-	 * @brief attempts to read data into given buffer
-	 * @param buffer a pointer to where data should be read to
-	 * @param numBytes the maximum number of bytes that can be read into @a buffer
-	 * @return true if successful
-	 */
+  /**
+   * @brief attempts to read data into given buffer
+   * @param buffer a pointer to where data should be read to
+   * @param numBytes the maximum number of bytes that can be read into @a buffer
+   * @return true if successful
+   */
   bool Recv (void *buffer, int numBytes);
 
   /** @brief gets if this socket is currently connected */
   bool IsConnected () const
-	{ return mIsConnected; }
+  { return mIsConnected; }
 
   /** @brief gets if this socket is currently not connected */
   bool IsNotConnected () const
-	{ return !mIsConnected; }
+  { return !mIsConnected; }
 
 private:
-	/**
-	 * @brief creates a socket based on the mProtocol
-	 * @return integer of the file descriptor for the created socket
-	 */
+  /**
+   * @brief creates a socket based on the mProtocol
+   * @return integer of the file descriptor for the created socket
+   */
   int CreateSocket ();
 
-	/**
-	 * @brief binds mSocketFD to portNum
-	 * @param portNum the port number to bind the socket to
-	 * @return return code of bind call, 0 if successful
-	 */
+  /**
+   * @brief binds mSocketFD to portNum
+   * @param portNum the port number to bind the socket to
+   * @return return code of bind call, 0 if successful
+   */
   int BindSocket (int portNum);
 
-	/**
-	 * @brief closes the socket and sets as not connected
-	 */
+  /**
+   * @brief closes the socket and sets as not connected
+   */
   void CloseSocket ();
 
-	/**
-	 * @brief listens and accepts TCP connections
-	 * @return true if successfully connected
-	 */
+  /**
+   * @brief listens and accepts TCP connections
+   * @return true if successfully connected
+   */
   bool ListenAndAccept ();
 
   /** host name receiving data */
@@ -106,13 +106,13 @@ private:
   int mPort;
   /** holds the connection type */
   eProtocol mProtocol;
-	/** holds the socket file descriptor */
+  /** holds the socket file descriptor */
   int mSocketFD;
-	/** indicates connection status */
+  /** indicates connection status */
   bool mIsConnected;
-	/** indicates if this is a host socket */
+  /** indicates if this is a host socket */
   bool mIsHostSocket;
-	/** holds host connection info */
+  /** holds host connection info */
   sockaddr_in mHostInfo;
 };
 
